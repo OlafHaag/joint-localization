2b8743ba98317375410662d5195ff4d59c66a3d7
Olaf Haag <haag.olaf@gmail.com>
2018-10-14 14:02:11 +0200

    intregrate stsc_ulti into stsc

--- joint_localization/stsc.py
+++ joint_localization/stsc.py
@@ -3,11 +3,13 @@ Zelnik-Manor, L., & Perona, P. (2005). Self-tuning spectral clustering.
 In Advances in neural information processing systems (pp. 1601-1608).
 Original Paper: https://papers.nips.cc/paper/2619-self-tuning-spectral-clustering.pdf
 """
+from itertools import groupby
+
 import numpy as np
 from functools import reduce
-from scipy.optimize import minimize
 
-from joint_localization.stsc_ulti import affinity_to_lap_to_eig, reformat_result, get_min_max
+from scipy.linalg import inv, sqrtm, eigh
+from scipy.optimize import minimize
 
 
 def generate_Givens_rotation(i, j, theta, size):
@@ -103,6 +105,35 @@ def get_rotation_matrix(X, C):
                    jac=True,
                    options={'disp': False})
     return opt.fun, reduce(np.dot, generate_U_list(ij_list, opt.x, C), np.eye(C))
+
+
+def reformat_result(cluster_labels, n):
+    zipped_data = zip(cluster_labels, range(n))
+    zipped_data = sorted(zipped_data, key=lambda x: x[0])
+    grouped_feature_id = [[j[1] for j in i[1]] for i in groupby(zipped_data, lambda x: x[0])]
+    return grouped_feature_id
+
+
+def affinity_to_lap_to_eig(affinity):
+    tril = np.tril(affinity, k=-1)
+    a = tril + tril.T
+    d = np.diag(a.sum(axis=0))
+    dd = inv(sqrtm(d))
+    l = dd.dot(a).dot(dd)
+    w, v = eigh(l)
+    return w, v
+
+
+def get_min_max(w, min_n_cluster, max_n_cluster):
+    if min_n_cluster is None:
+        min_n_cluster = 2
+    if max_n_cluster is None:
+        max_n_cluster = np.sum(w > 0)
+        if max_n_cluster < 2:
+            max_n_cluster = 2
+    if min_n_cluster > max_n_cluster:
+        raise ValueError('min_n_cluster should be smaller than max_n_cluster')
+    return min_n_cluster, max_n_cluster
 
 
 def self_tuning_spectral_clustering(affinity, min_n_cluster=None, max_n_cluster=None):
